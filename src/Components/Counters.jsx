bc077a62f887737616bad940bb4077d152a55909
Anton <goncharov@inbox.ru>
2021-08-26 23:17:40 +0300

    Finished first task

--- src/Components/Counters.jsx
+++ src/Components/Counters.jsx
@@ -1,19 +1,59 @@
 import Counter from './counter.jsx'
+import React, { useState } from 'react';
 
 
 const Counters = () => {
-  const usrs = [
-    { name: 'Anton', age: '23', id: 1 },
-    { name: 'Artur', age: '23' , id: 2},
-    { name: 'Vlad', age: '22' , id: 3},
+  const initialParametrs = [
+    { value: 0, name: '23', id: 1 },
+    { name: 'Artur', value: 4, id: 2 },
+    { name: 'Vlad', value: 5, id: 3 },
   ];
-  return ( 
+  const [counters, setCounters]  = useState(initialParametrs)
+
+
+  const increment = (counterId) => {
+    const newList = counters.map((counter) => {
+      if (counter.id === counterId) {
+        counter.value = counter.value  + 1;
+      }
+      return counter;
+    });
+    setCounters(newList);
+  };
+
+  const decrement = (counterId) => {
+    const newList = counters.map((counter) => {
+      if (counter.id === counterId){
+        counter.value = counter.value > 0 ? counter.value - 1 : 0
+      }
+      return counter
+    });
+    setCounters(newList);
+  }; 
+  
+  const deleteCounter = (counterId) => {
+    const newList = counters.filter((counter) => counter.id != counterId)
+    setCounters(newList)
+  };
+
+  const resetCounters = (initial) => {
+    setCounters(initial)
+  }
+  return (
     <>
-    {usrs.map((user) => {
-      return <Counter name ={user.name} age = {user.age} key = {user.id}>My children</Counter>
-    } )}
-    <h1>The end</h1>
-  </>
+      <button className="btn  btn-danger btn-sm mt-5 ms-3" onClick = {() => resetCounters(initialParametrs)}>Reset</button>
+      {counters.map((counter) => {
+        return (
+          <Counter
+            key={counter.id}
+            onDelete={deleteCounter}
+            onIncrement={increment}
+            onDecrement={decrement}
+            {...counter}
+          ></Counter>
+        );
+      })}
+    </>
   );
 }
 
--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,39 +1,32 @@
-import React, { useState } from 'react';
 import './counter.css';
 
-const Counter = (props) => {
-  let [counter, setCounter] = useState(0);
+
+const Counter = (props) => { 
 
   const startCount = () => {
-    return counter === 0 ? 'Ноль' : counter;
+    return props.value === 0 ? 'Ноль' : props.value;
   };
-  const increment = () => {
-    return setCounter(counter + 1);
-  };
-
-  const decrement = () => {
-    return counter <= 0 ? 'Ноль' : setCounter(counter - 1);
-  };
+  
 
   const getBageClass = () => {
     let classes = 'mx-3 badge bg-';
-    return classes += counter === 0 ? 'danger' : 'primary'
+    return classes += props.value === 0 ? 'danger' : 'primary'
   }
 
+  
+
   return (
-    <>
+    <div className="mb-9">
       <br></br>
-      <p>Name: {props.name}</p>
-      <p>Age: {props.age}</p>
-      <p>{props.children}</p>
       <span className={getBageClass()}>{startCount()}</span>
-      <button className="btn btn-secondary me-2 btn-sm" onClick={increment}>
+      <button className="btn btn-secondary me-2 btn-sm" onClick={() => props.onIncrement(props.id)}>
         Increment
       </button>
-      <button className="btn btn-secondary btn-sm" onClick={decrement}>
+      <button className="btn btn-secondary btn-sm" onClick= {() => props.onDecrement(props.id)}>
         Decrement
       </button>
-    </>
+      <button className = "btn btn-danger btn-sm mx-2" onClick = {() => props.onDelete(props.id)}>Delete</button>
+    </div>
   );
 };
 
